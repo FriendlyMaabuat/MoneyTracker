926eb84928073fce9bb4b2a6280cb8a81f095a99
FriendlyMaabuat <friendlymaabuat25@gmail.com>
2022-05-11 02:25:49 +0800

    Done

---
+++ src/assets/icons/index.js
@@ -0,0 +1,17 @@
+import Logoo from './Logo.svg';
+import IconBack from './icon-back.svg';
+import Pic from './pic.svg';
+import HomeActive from './HomeActive.svg';
+import HomeNotActive from './HomeNotActive.svg';
+import FrameActive from './FrameActive.svg';
+import FrameNotActive from './FrameNotActive.svg';
+
+export {
+  Logoo,
+  IconBack,
+  Pic,
+  HomeActive,
+  HomeNotActive,
+  FrameActive,
+  FrameNotActive,
+};
